2c6679d01d8af2ffe20743ca481a0edc43a86b8c
Pranil <pranilkhadka321@gmail>
2022-09-04 22:05:40 +0545

    third commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,9 @@
+from urllib import response
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET'])
+def status(request):
+    return Response(status=200)
